0d7073ebb99eca56c6c5fad7434430dca10ab660
ERROR938 <sebasten.orgue@gmail.com>
2025-02-18 20:43:24 +0100

    change token

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ client.once('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag}`);
 });
 
-client.login("MTM0MTQ2NzM4NDc4Mjc4MjQ3Ng.GK6b8P.UraIPeXRzbZtZLaZkfSg8i-FN_jtk_b-mpro3U");
+client.login("MTM0MTQ2NzM4NDc4Mjc4MjQ3Ng.GmV32g.o0mx6ljjJMiBtUUhYV9ryEBL79xk2klActuDN0");
